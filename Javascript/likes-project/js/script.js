58720f54d676d7c635d24a0f75156966dfea36c5
Franco_H <francoh.jobs@gmail.com>
2023-02-16 21:27:31 -0500

    3 like buttons incrementing other elements

---
+++ Javascript/likes-project/js/script.js
@@ -0,0 +1,26 @@
+function likeClick(likeButton) {
+    var currentLikeButton = likeButton;
+    var likeBtnElement = document.querySelector(currentLikeButton);
+    var currentVotes = likeBtnElement.innerHTML; 
+
+        console.log(currentLikeButton);
+        console.log(likeBtnElement);
+        console.log(currentVotes);
+
+    if (currentLikeButton == '#likes-1') {
+        likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
+    } else if (currentLikeButton == '#likes-2') {
+        likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
+    } else if (currentLikeButton == '#likes-3') {
+        likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
+    }
+
+
+
+
+    // var currentLikes = parseInt(currentLikes) + 1
+
+    // var currentLikes = someLikes.split(" ")[0]
+    
+    // element.innerText = currentLikes + " likes"
+}
