acb1aa64e501fcba0af7c1f6bdd66c63d444b4c6
Franco_H <francoh.jobs@gmail.com>
2023-02-16 22:07:05 -0500

    very simplified way to modify elements js

--- Javascript/likes-project/js/script.js
+++ Javascript/likes-project/js/script.js
@@ -1,26 +1,31 @@
-function likeClick(likeButton) {
-    var currentLikeButton = likeButton;
-    var likeBtnElement = document.querySelector(currentLikeButton);
-    var currentVotes = likeBtnElement.innerHTML; 
+// function likeClick(likeButton) {
+//     var currentLikeButton = likeButton;
+//     var likeBtnElement = document.querySelector(currentLikeButton);
+//     var currentVotes = likeBtnElement.innerHTML; 
 
-        console.log(currentLikeButton);
-        console.log(likeBtnElement);
-        console.log(currentVotes);
+//         console.log(currentLikeButton);
+//         console.log(likeBtnElement);
+//         console.log(currentVotes);
 
-    if (currentLikeButton == '#likes-1') {
-        likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
-    } else if (currentLikeButton == '#likes-2') {
-        likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
-    } else if (currentLikeButton == '#likes-3') {
-        likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
-    }
-
-
+//     if (currentLikeButton == '#likes-1') {
+//         likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
+//     } else if (currentLikeButton == '#likes-2') {
+//         likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
+//     } else if (currentLikeButton == '#likes-3') {
+//         likeBtnElement.innerHTML = +likeBtnElement.innerHTML +1;
+//     }
 
 
-    // var currentLikes = parseInt(currentLikes) + 1
 
-    // var currentLikes = someLikes.split(" ")[0]
+
+//     // var currentLikes = parseInt(currentLikes) + 1
+
+//     // var currentLikes = someLikes.split(" ")[0]
     
-    // element.innerText = currentLikes + " likes"
+//     // element.innerText = currentLikes + " likes"
+// }
+
+function likeClick(id) {
+    var like = document.querySelector(id);
+    like.innerText++ /* the ++ both converts the innerText to an integer AND increments!!!!! */
 }
